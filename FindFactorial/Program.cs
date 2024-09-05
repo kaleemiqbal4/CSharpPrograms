4474b343cf9113f58388a87177c0c75e46bed044
KALEEM IQBAL <olduser0071@gmail.com>
2024-09-06 02:02:12 +0530

    update d

---
+++ FindFactorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace FactorialFind;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        BigInteger result = Result.extraLongFactorials(n);
+        Console.WriteLine(result);
+    }
+}
+
+public class Result
+{
+    public static BigInteger extraLongFactorials(int n)
+    {
+        if (n == 0 || n == 1) 
+        { 
+            return 1; 
+        }
+        return n * extraLongFactorials(n - 1);
+    }
+}
+
+
---
+++ SortArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SortArrays;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 32, 2, 87, 0, 99, 120, 874, 3 };
+        SortArrayResult sortArrayResult = new SortArrayResult();
+       var result = sortArrayResult.SortArrayMethod(arr);
+        Console.WriteLine(string.Join(", ", result));
+        Console.ReadLine();
+    }
+}
+
+public class SortArrayResult
+{
+    public int[] SortArrayMethod(int[] arr) 
+    {
+        for (int i = 0; i < arr.Length; i++) 
+        { 
+            for(int j = i + 1; j<arr.Length; j++)
+            {
+                int temp = 0;
+                if(arr[i] > arr[j])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
